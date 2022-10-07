4d16aafc09b884227a71bb89f1af6880d886636f
MicheleTornetta <112029252+MicheleTornetta@users.noreply.github.com>
2022-10-07 17:30:29 -0400

    local storage button and flex that needs adjusted

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,41 +1,60 @@
-
 //set up for current date
 
-$("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
+$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 //console.log($("#currentDate"));
 
-/*debugger;*/  //helps go through each line to look for problems
+/*debugger;*/ //helps go through each line to look for problems
 
-for(var hour = 9; hour <= 17; hour++){
-   //Create timeblocks divs, text box and button
-   let rowBox = document.createElement("div");
-   let timeBox = document.createElement("div");
-   let inputBox = document.createElement("input");
-   let saveBox = document.createElement("button" );
-   //class to allow css changes
-   rowBox.classList.add("rowBox");
+for (let hour = 9; hour <= 17; hour++) {
+  //Create timeblocks divs, text box and button
+  let rowBox = document.createElement("div");
+  let timeBox = document.createElement("div");
+  let inputBox = document.createElement("input");
+  let saveBtn = document.createElement("button");
 
-   //Adding children to the parent container
-   $(".container").append(rowBox);
+  //classes to allow css changes
+  rowBox.classList.add("rowBox");
+  saveBtn.classList.add("saveBox");
+  inputBox.classList.add("inputBox");
+  timeBox.classList.add("timeBox");
 
-   //Calendar Start Time
-   let startTime = moment().set({'hour': hour, 'minute': 0, 'seconds': 0}).format('LT');
-   /*console.log(startTime);*/
+  //Adding children to the parent container
+  $(".container").append(rowBox);
 
-   timeBox.innerText = startTime
-   saveBox.innerText = "Save"
+  //Calendar Start Time
+  let startTime = moment()
+    .set({ hour: hour, minute: 0, seconds: 0 })
+    .format("LT");
+  /*console.log(startTime);*/
+ 
+  // adding text to the timeblock rows
+  timeBox.innerText = startTime;
+  saveBtn.innerText = "Save";
+  inputBox.value = getTask(hour);
 
-   //add to the rowbox
-   $(rowBox).append(timeBox);
-   $(rowBox).append(inputBox);
-   $(rowBox).append(saveBox);
-  
-   //add button to saveBox
+  //add blocks to the rowbox
+  $(rowBox).append(timeBox);
+  $(rowBox).append(inputBox);
+  $(rowBox).append(saveBtn);
 
+  //Make Save button save hourly task
 
-   function saveScore(score) {
-   let currentScores = getScores();
-   currentScores.push(score);
-   localStorage.setItem("savedScores", JSON.stringify(currentScores)); //turns it into a string
- }
+ $(saveBtn).click(function (event) {
+   event.preventDefault();
+   console.log(inputBox.value);
+   saveTask(inputBox.value, hour);
+  });
 }
+
+//saves the task to local storage
+function saveTask(input, hour) {
+   localStorage.setItem("tasks" + hour, JSON.stringify(input));
+ }
+ //reads from local storage and gives the result
+ function getTask(hour) {
+   return JSON.parse (localStorage.getItem("tasks" + hour));
+ }
+
+//get current hour??????????????
+/*var time = parseInt(currentDateTime.format("h"));
+console.log(time)*/
--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # work-day-scheduler
 
---save events for each hour of the day 
---feature dynamically updated HTML and CSS powered by jQuery
+    DONE--save events for each hour of the day 
+    DONE--feature dynamically updated HTML and CSS powered by jQuery
     DONE--current day is displayed at the top of the calendar
---scroll down I am presented with --timeblocks for standard business hours
+    DONE--scroll down I am presented with --timeblocks for standard business hours
 --each timeblock is color coded to indicate whether it is in the past, present, or future
---click into a timeblock enter an event
---click the save button for that timeblock
---event is saved in local storage
---WHEN I refresh the page THEN the saved events persist
+    DONE--click into a timeblock enter an event
+    DONE--click the save button for that timeblock
+    DONE--event is saved in local storage
+    DONE--WHEN I refresh the page THEN the saved events persist
 
 --https://momentjs.com/
 
